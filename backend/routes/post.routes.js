a7563651d74ee2eeaac6f676b0f084e4c2dd1c4a
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2023-05-11 23:34:16 +0300

    Add files via upload

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -5,4 +5,27 @@ router.get("/", (req, res) => {
     res.json({ message: "voici les données" });
 });
 
+router.post("/", (req, res) => {
+    console.log(req.body)
+    res.json({ message: req.body.message });
+});
+
+router.put("/:id", (req,res) => {
+    res.json({messageId: req.params.id})
+})
+
+router.delete("/:id", (req, res) => {
+    res.json({message: "Post supprime id: " + req.params.id})
+})
+
+router.patch("/like-post/:id", (req, res) => {
+    res.json({message: "Post liké : id : " + req.params.id })
+})
+
+router.patch("/dislike-post/:id", (req, res) => {
+    res.json({message: "Post disliké : id : " + req.params.id })
+})
+
+
+
 module.exports = router
